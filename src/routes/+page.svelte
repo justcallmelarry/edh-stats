242125a247b12ac90b922ab3cabb183367bde8f3
Lauri Vuorela <lauri.vuorela@gmail.com>
2025-02-21 22:42:12 +0100

    update copy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,8 +15,8 @@
 	<div class="hero-content text-center">
 		<div class=" max-w-3xl">
 			<h1 class="text-5xl font-bold">EDH Stats!</h1>
-			<p class="py-6">Stats to prove that Sossen plays too strong decks.</p>
-			<button on:click={onClick} class="btn btn-primary">Get Started</button>
+			<p class="py-6">Stats to prove that Sossen deserves being treated as archenemy.</p>
+			<button on:click={onClick} class="btn btn-primary">Go to stats</button>
 		</div>
 	</div>
 </div>
