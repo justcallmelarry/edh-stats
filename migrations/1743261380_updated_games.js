3a0d7f0cd323e80b1f78d8f94bacbad7956029d3
Lauri Vuorela <lauri.vuorela@gmail.com>
2025-03-29 17:07:55 +0100

    more migrations

---
+++ migrations/1743261380_updated_games.js
@@ -0,0 +1,28 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_879072730")
+
+  // update collection data
+  unmarshal({
+    "createRule": "@request.auth.id != \"\" && \n@collection.games.playgroup != null &&\n(@request.auth.id ?= @collection.playgroups.members.id || @request.auth.id = @collection.playgroups.owner)",
+    "deleteRule": "@request.auth.id != \"\" && \n@collection.games.playgroup != null &&\n(@request.auth.id = @collection.playgroups.owner)",
+    "listRule": "@request.auth.id != \"\" && \n@collection.games.playgroup != null &&\n(@request.auth.id ?= @collection.playgroups.members.id || @request.auth.id = @collection.playgroups.owner)",
+    "updateRule": "@request.auth.id != \"\" && \n@collection.games.playgroup != null &&\n(@request.auth.id ?= @collection.playgroups.members.id || @request.auth.id = @collection.playgroups.owner)",
+    "viewRule": "@request.auth.id != \"\" && \n@collection.games.playgroup != null &&\n(@request.auth.id ?= @collection.playgroups.members.id || @request.auth.id = @collection.playgroups.owner)"
+  }, collection)
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_879072730")
+
+  // update collection data
+  unmarshal({
+    "createRule": null,
+    "deleteRule": null,
+    "listRule": null,
+    "updateRule": null,
+    "viewRule": null
+  }, collection)
+
+  return app.save(collection)
+})
